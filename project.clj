12a9f6c2a91cfea3c46b512bc4a89b5505e1193b
Søren Knudsen <soeren.knudsen@formpipe.com>
2016-03-25 21:37:37 +0100

    Update project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject linden "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "An library for generating L-system structures"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
